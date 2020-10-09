3a7ce69e7ccfb2358287f6279845e957f4a91aa5
Mattys <mattysgervais@gmail.com>
2020-10-09 19:43:56 -0400

    Fix

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    Transform[] allChildren = GetComponentsInChildren<Transform>();
+    Transform[] allChildren;
     List<GameObject> SpawnPoints = new List<GameObject>();
     //Spawn sets
     private int zone;  //1 foret, 2 champs, 3 desert, 4 jungle, 5 roche
+    private float chanceRes, chanceMob;
+    private GameObject RessourceDrop;
     
-    
-    public Tile(int _zone,float chanceRes, float chanceMob)
+    public Tile(int _zone,float _chanceRes, float _chanceMob)
     {
         zone = _zone;
+        chanceRes = _chanceRes;
+        chanceMob = _chanceMob;
     }
     
     // Start is called before the first frame update
     void Start()
     {
+        allChildren = GetComponentsInChildren<Transform>();
         foreach (Transform child in allChildren)
         {
             SpawnPoints.Add(child.gameObject);
-            if (15 < Random.Range(0, 100))
+            if (chanceRes < Random.Range(1, 100))
             {
-                
+                Instantiate(RessourceDrop(zone), child.transform.position, Quaternion.identity);
             }
         }
     }
