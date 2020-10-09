ec48f6344e82210522cc5666dbbf21e0c9175112
Antoine T-S <anto291100@gmail.com>
2020-10-09 16:13:12 -0400

    toutes les posibilités

--- Assets/Scripts/static/AlchemyValues.cs
+++ Assets/Scripts/static/AlchemyValues.cs
@@ -9,13 +9,54 @@ public static class AlchemyValues
     public static List<Recipe> remainingRecipes = new List<Recipe>();
     public static List<Recipe> finishedRecipes = new List<Recipe>();
     public static Item[] materialPool = {};
+    private static int[] itemsAt;
 
-    public static void PopulateRecipes()
+
+    public static void PopulateRecipes(int materialPerRecipe)
     {
+        itemsAt = new int[materialPerRecipe]; 
+
+
+        while (itemsAt[0] < materialPool.Length)
+        {
+            for(int i = itemsAt[materialPerRecipe-1]; i < materialPool.Length; i++)
+            {
+                Item[] newRecipe = new Item[materialPerRecipe];
+                for(int j = 0; j < materialPerRecipe; j++)
+                {
+                    newRecipe[j] = materialPool[itemsAt[j]];
+                }
+                nextDigit(materialPerRecipe - 1);
+            }
+            
         
-        //TODO
+        }
+
+        Debug.Log(remainingRecipes.Count);
         
     }
+
+    private static void nextDigit(int pos)
+    {
+        if(pos > 0)
+        {
+            if(pos-1 < materialPool.Length - 1)
+            {
+                itemsAt[pos - 1]++;
+                for(int i = pos; i < itemsAt.Length; i++)
+                {
+                    itemsAt[i] = itemsAt[pos - 1];
+                }
+            }
+            else
+            {
+                nextDigit(pos - 1);
+            }
+        }
+
+    }
+
+    
 
     public static void FinishRecipe(Recipe r)
     {
