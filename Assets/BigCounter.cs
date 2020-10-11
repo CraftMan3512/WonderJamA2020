2e9749751689483ca282a5fbc1e177df5ba0698d
Memoh <CraftMan3512@gmail.com>
2020-10-11 10:25:21 -0400

    end of day Timer

---
+++ Assets/BigCounter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BigCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private bool started = false;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (DayTime.timePerDay - DayTime.time < 4)
+        {
+
+            if (!started)
+            {
+
+                started = true;
+                StartCoroutine(StartCDown());   
+                
+            }
+
+        } 
+        
+    }
+
+    public IEnumerator StartCDown()
+    {
+
+
+        GetComponent<TextMeshProUGUI>().enabled = true;
+        while (true)
+        {
+         
+            GetComponent<TextMeshProUGUI>().text = ((int)(DayTime.timePerDay - DayTime.time)).ToString();
+            yield return new WaitForSeconds(1f);
+            
+        }
+
+    }
+    
+}
