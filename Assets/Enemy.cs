43e1e536fa1dd21d4cd1e6e35ccb1e581c5ca17a
Antoine T-S <anto291100@gmail.com>
2020-10-11 12:43:21 -0400

    Build Gagnant

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -72,6 +72,8 @@ public class Enemy : MonoBehaviour
                         closestDirection = direction;
                     }
                 }
+                else
+                    allPlayers = GameObject.FindGameObjectsWithTag("Player");
             }
             
             //Chase
