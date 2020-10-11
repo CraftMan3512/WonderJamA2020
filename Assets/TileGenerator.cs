6e07a729e1d51f4ce3dc60156c9c165005cefeec
Antoine T-S <anto291100@gmail.com>
2020-10-11 12:13:29 -0400

    fix

--- Assets/TileGenerator.cs
+++ Assets/TileGenerator.cs
@@ -40,7 +40,7 @@ public class TileGenerator : MonoBehaviour
         {
             GameObject.Find("ExplorationTargetGroup").transform.position = new Vector3(AlchemyValues.posX-2.3f, 0,-11);
         }
-        endLength = 12+NumberOfTilesPerZone * 6 - 16;//todo regarder si le 10 est correct
+        endLength = 12+NumberOfTilesPerZone * 12 - 16;//todo regarder si le 10 est correct
         NumberOfTilesPerZone = DayTime.maxDays * 12;
         LastTilePos = 2; //6-4
         CurrZone = 1;
