18c0b4a75edecd046b45b6339950623dbe9c8388
Antoine T-S <anto291100@gmail.com>
2020-10-11 08:52:51 -0400

    Swap team fixed

--- Assets/TeamChooseController.cs
+++ Assets/TeamChooseController.cs
@@ -45,9 +45,13 @@ public class TeamChooseController : MonoBehaviour
             if (!(manette.leftStick.x < 0.5 && manette.leftStick.x > -0.5)) MoveToTeam();
             if (manette.aButton.wasPressedThisFrame) Ready();
             if (manette.bButton.wasPressedThisFrame) UnReady();
-        }else if (GameObject.Find("Transition").GetComponent<Transition>().endPauseTime <= 0.2f)
+        }else if (GameObject.Find("Transition").GetComponent<Transition>().endPauseTime <= 0.2f && DayTime.day == 1)
         {
             team = Teams.Exploration;
+            Ready();
+        }else if (GameObject.Find("Transition").GetComponent<Transition>().endPauseTime <= 0.2f)
+        {
+            
             Ready();
         }
 
